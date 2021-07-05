4eb259f191ad30a5da02d153ebc620215ed90c4e
Melanie Stuart <mmooradian90@yahoo.com>
2021-07-05 12:45:15 -0400

    importing schema and setting up post request.

--- pordbfront/pordbbackend/routes/routes.js
+++ pordbfront/pordbbackend/routes/routes.js
@@ -1,9 +1,23 @@
 const { response } = require('express')
 const express = require('express')
 const router = express.Router()
+const contributeTemplateCopy = require('../models/contributemodels')
 
 router.post('/contribute', (request, response) =>{
-    response.send('send')
+    const contributedTemplate = new contributeTemplateCopy({
+        companyName:request.body.companyName,
+        link:request.body.link,
+        reportEmailWebform:request.body.reportEmailWebform,
+        termsOfServiceIpPolicy:request.body.termsOfServiceIpPolicy,
+        comments:request.body.comments
+    })
+    contributedTemplate.save()
+    .then(data =>{
+        respons.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
+    })
 })
 
 
