0cf3dd5ecd418a31310fdd0097ab07c4ebd8c558
Melanie Stuart <mmooradian90@yahoo.com>
2021-07-24 20:30:47 -0400

    adding get routes

--- pordbfront/pordbbackend/server.js
+++ pordbfront/pordbbackend/server.js
@@ -8,7 +8,21 @@ const cors = require('cors')
 
 dotenv.config()
 
-mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log("Database conected"))
+mongoose.connect(process.env.DATABASE_ACCESS, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}, () => console.log("Database conected"))
+
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   });
+
 
 app.use(express.json())
 app.use(cors())
