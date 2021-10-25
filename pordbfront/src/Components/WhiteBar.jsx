beb15e7cac4b9b30d565383f004f73f946474cce
M Stuart <67798273+MStuart90@users.noreply.github.com>
2021-10-25 17:53:28 -0400

    last component

---
+++ pordbfront/src/Components/WhiteBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WhiteBar.css';
+
+const WhiteBar = (props) => {
+  return (
+    <div className='whiteBar'>
+      <h1> Report Intellectual Property Abuse</h1>
+      <h6> Protecting Brand Owners, Rightsholders And Creators </h6>
+    </div>
+  );
+}
+
+export default WhiteBar;
