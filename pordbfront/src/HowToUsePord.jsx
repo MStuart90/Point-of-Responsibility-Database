25e970ce523ebbd3f21cb6c2844ec59e877df240
M Stuart <67798273+MStuart90@users.noreply.github.com>
2021-10-26 19:50:50 -0400

    catching local up

---
+++ pordbfront/src/HowToUsePord.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HowToUsePord = (props) => {
+  return (
+    <div>
+      <h1>How To Use Pord</h1>
+      <p>lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor inc, you, sed do eiusmod tempor inc</p>
+    </div>
+  );
+}
+
+export default HowToUsePord;
