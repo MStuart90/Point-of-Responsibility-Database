383265265f317ab9b6c496cb46e556c703c25f14
Matt Hinegardner <matt.hinegardner@oracle.com>
2021-11-02 12:29:42 -0400

    clean up and added logo

--- pordbfront/src/Components/Navbar.jsx
+++ pordbfront/src/Components/Navbar.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import "./Navbarx.css";
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../logo.png';
 
-  
-const Navbarx = (props) => {
+const Navbarx = () => {
     return (
-      <div className="header wide">
-        <div>
-            <img alt="logo" src="logo.svg" />
-            <Link className="NavbarBrand NavLink" to="/"  >
-                <span>P</span>oints <span>O</span>f <span>R</span>esponsibility <span>D</span>atabase
-            </Link>
+        <div className='header wide'>
+            <div>
+                <Link className='NavbarBrand NavLink' to='/'>
+                    <img alt='logo' className='logo' src={logo} />
+                    <span>P</span>oints <span>O</span>f <span>R</span>
+                    esponsibility <span>D</span>atabase
+                </Link>
+            </div>
+            <div id='rightItms' className='header'>
+                <div className='NavItem'>
+                    <Link className='NavLink' to='/SignIn'>
+                        Login
+                    </Link>
+                </div>
+                <div className='NavItem'>
+                    <Link className='NavLink' to='/Contribute'>
+                        Contribute
+                    </Link>
+                </div>
+                <div className='NavItem'>
+                    <Link className='NavLink' to='/HowToUsePord'>
+                        How To Use <span>Pord</span>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div id="rightItms" className="header">
-            <div className="NavItem">
-                <Link className="NavLink" to="/SignIn">Login</Link>
-            </div>
-            <div className="NavItem">
-                <Link className="NavLink" to="/Contribute">Contribute</Link>
-            </div>
-            <div className="NavItem">
-                <Link className="NavLink" to="/HowToUsePord">How To Use <span>Pord</span></Link>
-            </div>
-        </div>
-      </div>
     );
-  };
-  
-  export default Navbarx;
+};
+
+export default Navbarx;
